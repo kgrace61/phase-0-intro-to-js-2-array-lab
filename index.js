58aa40d810eb9015282e914b7e787e2e02b37545
Kristen <kgrace61@gmail.com>
2024-02-04 17:33:19 -0600

    creates myfile.md

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution here!
+const cats= ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(Agnes) {
+    cats.push(Agnes);
+}
+
+function destructivelyPrependCat(Orangey) {
+    cats.unshift(Orangey);
+}
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+function appendCat(Snowy) {
+    return [...cats, Snowy];
+}
+function prependCat(Midnight) {
+    return [Midnight, ...cats]
+}
+function removeLastCat() {
+    return cats.slice(0, cats.length-1);
+}
+function removeFirstCat() {
+    return cats.slice(1);
+}
